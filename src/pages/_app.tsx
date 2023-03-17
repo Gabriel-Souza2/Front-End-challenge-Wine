085b697bb80dddc69a56c2d65add71fa346150e7
Gabriel-Souza2 <gabriel.souza2@hotmail.com.br>
2023-03-17 16:34:47 -0300

    :lipstick: feat: Reset CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "@/styles/global/theme";
+import { Reset } from "@/styles/global/reset";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Reset />
       <Component {...pageProps} />
     </ThemeProvider>
   );
