8fa673ec9293c5b9a89918c37b7d0501e94e26e9
Gabriel-Souza2 <gabriel.souza2@hotmail.com.br>
2023-03-20 22:00:41 -0300

    :lipstick: feat: Criando filtro de preços

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,3 +1,45 @@
+import { Container, CustomRadio } from "./styles";
+
 export function Filter() {
-  return <h3>Aside menu</h3>;
+  return (
+    <Container>
+      <h4>Refine sua busca</h4>
+      <span className="byPrice">Por Preço</span>
+      <label>
+        <input type="radio" id="p_40" name="by_price" value="p_40" />
+        <CustomRadio>
+          <div className="checked"></div>
+        </CustomRadio>
+        <span>Até R$40</span>
+      </label>
+      <label>
+        <input type="radio" id="p_40_60" name="by_price" value="p_40_60" />
+        <CustomRadio>
+          <div className="checked"></div>
+        </CustomRadio>
+        <span>R$40 A R$60</span>
+      </label>
+      <label>
+        <input type="radio" id="p_100_200" name="by_price" value="p_100_200" />
+        <CustomRadio>
+          <div className="checked"></div>
+        </CustomRadio>
+        <span>R$100 A R$200</span>
+      </label>
+      <label>
+        <input type="radio" id="p_200_500" name="by_price" value="p_200_500" />
+        <CustomRadio>
+          <div className="checked"></div>
+        </CustomRadio>
+        <span>R$200 A R$500</span>
+      </label>
+      <label>
+        <input type="radio" id="p_500" name="by_price" value="p_500" />
+        <CustomRadio>
+          <div className="checked"></div>
+        </CustomRadio>
+        <span>Acima de R$500</span>
+      </label>
+    </Container>
+  );
 }
---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const Container = styled.aside`
+  display: flex;
+  flex-direction: column;
+
+  h4 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 3.2rem;
+  }
+
+  .byPrice {
+    font-size: 1.8rem;
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors["gray-600"]};
+    margin-bottom: 1.6rem;
+  }
+
+  input[type="radio"] {
+    display: none;
+  }
+
+  input[type="radio"]:checked ~ div {
+    border-color: ${({ theme }) => theme.colors["purple-500"]};
+
+    .checked {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+
+  input[type="radio"]:hover ~ div {
+    border-color: ${({ theme }) => theme.colors["purple-500"]};
+    opacity: 0.5;
+
+    .checked {
+      transform: scale(100%);
+      opacity: 0.5;
+    }
+  }
+
+  label {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-bottom: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const CustomRadio = styled.div`
+  width: 1.6rem;
+  height: 1.6rem;
+  border-radius: 999px;
+
+  border: 2px solid ${({ theme }) => theme.colors["gray-300"]};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .checked {
+    width: 1rem;
+    height: 1rem;
+    border-radius: 999px;
+    background-color: ${({ theme }) => theme.colors["purple-500"]};
+    transform: scale(0);
+    opacity: 0;
+    transition: all 0.2s ease-in;
+  }
+`;
