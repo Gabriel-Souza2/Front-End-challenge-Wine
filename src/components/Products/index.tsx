41c8ffaf7a810b476bc4e49f3fecfdf2b4a3b437
Gabriel-Souza2 <gabriel.souza2@hotmail.com.br>
2023-03-22 00:03:23 -0300

    :recycle: refactor: Refatorando produtos

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,4 +1,4 @@
-import { Container, ProductsList, Product, AddButton } from "./styles";
+import { Container, ProductsList } from "./styles";
 
 export function Products() {
   return (
@@ -7,27 +7,7 @@ export function Products() {
         <span>49</span> produtos encontrados
       </p>
 
-      <ProductsList>
-        <Product>
-          <div className="product-wrapper">
-            <img src="/wine.png" alt="vinho" />
-            <p>Bacalhôa Meia Pipa Private Selection Castelão Syrah 2014</p>
-            <div className="discount">
-              <span className="old-price">R$ 37,40</span>
-              <span className="total-discount">60% OFF</span>
-            </div>
-            <div className="price-wrapper">
-              <span className="partner">Sócio wine</span>
-              <span className="price">
-                <span className="symbol">R$</span>
-                <span className="integer">30</span>,00
-              </span>
-            </div>
-            <span className="not-partner">Não sócio R$ 37,40</span>
-          </div>
-          <AddButton>Adicionar</AddButton>
-        </Product>
-      </ProductsList>
+      <ProductsList></ProductsList>
     </Container>
   );
 }
